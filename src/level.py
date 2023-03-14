5a74ff76be51884ff91bb1e3211b4c1842980009
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-03-15 00:41:27 +0100

    Updated code

--- src/level.py
+++ src/level.py
@@ -7,22 +7,20 @@ class Level:
 
 	def setup(self) -> None:
 		self.player = pygame.sprite.GroupSingle()
-		self.player.add(Player((300, 300)))
-
+		self.blocks = pygame.sprite.Group()
 # TO REMOVE vvvv
+		self.player.add(Player((300, 300)))
 		floor = pygame.sprite.Sprite()
 		floor.image = pygame.Surface((1200, 300))
 		floor.image.fill('gray')
 		floor.rect = floor.image.get_rect(topleft=(0, 400))
-
-		self.floor_group = pygame.sprite.GroupSingle()
-		self.floor_group.add(floor)
+		self.blocks.add(floor)
 # TO REMOVE ^^^^
 
 	def run(self, dt) -> None:
 		self.display_surface.fill('black')
 
-		self.floor_group.draw(self.display_surface)
+		self.blocks.draw(self.display_surface)
 
 		self.player.update(dt)
 		self.player.draw(self.display_surface)
